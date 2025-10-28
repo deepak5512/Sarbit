5a4587a13db6476d24f6400581986666ee5f5725
Deepak Bhatter <120327568+deepak5512@users.noreply.github.com>
2025-10-29 00:41:22 +0530

    Home Page Created

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        hostname: "ik.imagekit.io",
+      },
+      {
+        hostname: "images.unsplash.com",
+      },
+      {
+        hostname: "i.imgur.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
