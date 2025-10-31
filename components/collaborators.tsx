470a051d459e050ae619824983150d9f292db71c
Deepak Bhatter <120327568+deepak5512@users.noreply.github.com>
2025-10-31 23:59:21 +0530

    Collaborator Issue Fixed

--- components/collaborators.tsx
+++ components/collaborators.tsx
@@ -1,157 +1,79 @@
 import Image from "next/image";
-import { InfiniteSlider } from "./ui/infinite-slider";
-import { ProgressiveBlur } from "./ui/progressive-blur";
+import Link from "next/link";
+
+const data = [
+  {
+    name: "AIIMS",
+    src: "/collaborators/aiims.png",
+    href: "https://www.aiims.edu/",
+  },
+  {
+    name: "BIRAC",
+    src: "/collaborators/birac.png",
+    href: "https://www.birac.nic.in/",
+  },
+  {
+    name: "IIT Jodhpur",
+    src: "/collaborators/iitj.png",
+    href: "https://www.iitj.ac.in/",
+  },
+  {
+    name: "JCKIF",
+    src: "/collaborators/jckif.png",
+    href: "https://jckif.iitj.ac.in/",
+  },
+  {
+    name: "MedTech",
+    src: "/collaborators/medtech.png",
+    href: "https://medtech.iitj.ac.in/",
+  },
+  {
+    name: "SINE",
+    src: "/collaborators/sine.png",
+    href: "https://sine.iitj.ac.in/",
+  },
+];
 
 const Collaborators = () => {
   return (
-    <section className="bg-background pt-16 pb-16 md:pt-32 md:pb-32">
-      <div className="group relative m-auto max-w-6xl px-6">
-        <div className="flex flex-col items-center md:flex-row">
-          <div className="md:max-w-44 md:border-r md:pr-6">
-            <p className="text-end text-sm">Powering with best collaborators</p>
-          </div>
-
-          <div className="relative py-6 md:w-[calc(100%-11rem)]">
-            <InfiniteSlider speedOnHover={20} speed={60} gap={100}>
-              {/* AIIMS */}
-              <a
-                href="https://www.aiims.edu/"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="flex items-center justify-center"
-              >
-                <div className="relative flex h-20 w-20 items-center justify-center">
+    <section className="pt-16 pb-16 md:pb-32">
+      <div className="group relative m-auto max-w-5xl px-6">
+        <div className="mx-auto mt-12 grid max-w-2xl grid-cols-4 gap-x-12 gap-y-8 transition-all duration-500 sm:gap-x-16 sm:gap-y-14">
+          {data.map((collaborator, index) => (
+            <div key={index} className="flex">
+              {collaborator.name === "MedTech" ? (
+                <Link
+                  href={collaborator.href}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
                   <Image
-                    src="/collaborators/aiims.png"
-                    alt="AIIMS Logo"
-                    fill
-                    className="object-contain"
+                    src={collaborator.src}
+                    alt={`${collaborator.name} Logo`}
+                    height={128}
+                    width={128}
+                    className="mx-auto h-16 w-fit dark:invert"
                     priority
                   />
-                </div>
-              </a>
-
-              {/* BIRAC */}
-              <a
-                href="https://www.birac.nic.in/"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="flex items-center justify-center"
-              >
-                <div className="relative flex h-20 w-20 items-center justify-center">
+                </Link>
+              ) : (
+                <Link
+                  href={collaborator.href}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
                   <Image
-                    src="/collaborators/birac.png"
-                    alt="BIRAC Logo"
-                    fill
-                    className="object-contain"
+                    src={collaborator.src}
+                    alt={`${collaborator.name} Logo`}
+                    height={128}
+                    width={128}
+                    className="mx-auto h-16 w-fit"
                     priority
                   />
-                </div>
-              </a>
-
-              {/* IIT Jodhpur */}
-              <a
-                href="https://www.iitj.ac.in/"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="flex items-center justify-center"
-              >
-                <div className="relative flex h-20 w-20 items-center justify-center">
-                  <Image
-                    src="/collaborators/iitj.png"
-                    alt="IIT Jodhpur Logo"
-                    fill
-                    className="object-contain"
-                    priority
-                  />
-                </div>
-              </a>
-
-              {/* JCKIF */}
-              <a
-                href="https://jckif.iitj.ac.in/"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="flex items-center justify-center"
-              >
-                <div className="relative flex h-20 w-20 items-center justify-center">
-                  <Image
-                    src="/collaborators/jckif.png"
-                    alt="JCKIF Logo"
-                    fill
-                    className="object-contain"
-                    priority
-                  />
-                </div>
-              </a>
-
-              {/* MedTech */}
-              <a
-                href="https://medtech.iitj.ac.in/"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="flex items-center justify-center"
-              >
-                <div className="relative flex h-20 w-20 items-center justify-center">
-                  <Image
-                    src="/collaborators/medtech.png"
-                    alt="MedTech Logo"
-                    fill
-                    className="object-contain dark:invert"
-                    priority
-                  />
-                </div>
-              </a>
-
-              {/* SINE */}
-              <a
-                href="https://sineiitb.org/"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="flex items-center justify-center"
-              >
-                <div className="relative flex h-20 w-20 items-center justify-center">
-                  <Image
-                    src="/collaborators/sine.png"
-                    alt="Sine Logo"
-                    fill
-                    className="object-contain"
-                    priority
-                  />
-                </div>
-              </a>
-
-              {/* TISC */}
-              <a
-                href="https://tisc.iitj.ac.in/"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="flex items-center justify-center"
-              >
-                <div className="relative flex h-20 w-20 items-center justify-center">
-                  <Image
-                    src="/collaborators/tisc.png"
-                    alt="TISC Logo"
-                    fill
-                    className="object-contain"
-                    priority
-                  />
-                </div>
-              </a>
-            </InfiniteSlider>
-
-            {/* Blurs on both sides */}
-            <ProgressiveBlur
-              className="pointer-events-none absolute top-0 left-0 h-full w-10"
-              direction="left"
-              blurIntensity={1}
-            />
-            <ProgressiveBlur
-              className="pointer-events-none absolute top-0 right-0 h-full w-10"
-              direction="right"
-              blurIntensity={1}
-            />
-          </div>
+                </Link>
+              )}
+            </div>
+          ))}
         </div>
       </div>
     </section>
