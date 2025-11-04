3a1e650339a7fa1bc80a8f8f6407ba1722101372
Deepak Bhatter <120327568+deepak5512@users.noreply.github.com>
2025-11-04 20:40:00 +0530

    Product Images Modified

--- data/productData.tsx
+++ data/productData.tsx
@@ -10,14 +10,14 @@ export const productsData = {
     title: "Micro/Nano Patterned Devices",
     description:
       "Utilizing photolithography, wet-etching, and lift-off methods to generate electrodes, sensors, and biosensors from patterned metal films.",
-    image: "/products/micro.jpg",
+    image: "/products/micro.png",
     alt: "Micro/Nano Patterned Devices",
   },
   "item-3": {
     title: "Metals & Dielectric Coatings",
     description:
       "Discover our proficiency in RF & DC Sputtering, e-beam and thermal evaporation of various metals & dielectrics.",
-    image: "/products/metal.jpg",
+    image: "/products/metal.png",
     alt: "Metals & Dielectric Coatings",
   },
   "item-4": {
