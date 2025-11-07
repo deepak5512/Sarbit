976dcf3b5616a524906ad07e123ba6a18b9e1d06
Deepak Bhatter <120327568+deepak5512@users.noreply.github.com>
2025-11-08 01:06:05 +0530

    Text Modified

--- components/products.tsx
+++ components/products.tsx
@@ -105,16 +105,15 @@ import { ProductCard } from "./ui/product-card";
 
 export default function Products() {
   return (
-    <section className="mx-auto max-w-6xl px-6 py-16 md:py-32">
-      {/* Heading Section */}
+    <section className="mx-auto max-w-5xl px-6 py-16 md:py-32">
       <div className="mb-12">
-        <h2 className="text-3xl font-semibold text-gray-900 sm:text-4xl dark:text-gray-100">
-          Building the next generation
+        <h2 className="text-3xl font-semibold text-neutral-700 sm:text-4xl dark:text-gray-100">
+          Empowering innovation through
           <br />
-          <span className="font-bold text-gray-900 dark:text-gray-50">
-            of{" "}
-            <span className="text-gray-800 dark:text-white">
-              AI-powered Marketing Tools
+          <span className="font-bold text-neutral-900 dark:text-gray-50">
+            next-generation{" "}
+            <span className="text-neutral-800 dark:text-white">
+              Semiconductor & Sensor Solutions
             </span>
           </span>
         </h2>
--- components/services.tsx
+++ components/services.tsx
@@ -3,22 +3,20 @@ import { ProductCard } from "./ui/product-card";
 
 export default function Services() {
   return (
-    <section className="mx-auto max-w-6xl px-6 py-16 md:py-32">
-      {/* Heading Section */}
+    <section className="mx-auto max-w-5xl px-6 py-16 md:py-32">
       <div className="mb-12">
-        <h2 className="text-3xl font-semibold text-gray-900 sm:text-4xl dark:text-gray-100">
-          Building the next generation
+        <h2 className="text-3xl font-semibold text-gray-700 sm:text-4xl dark:text-gray-100">
+          Accelerating innovation through
           <br />
           <span className="font-bold text-gray-900 dark:text-gray-50">
-            of{" "}
+            cutting-edge{" "}
             <span className="text-gray-800 dark:text-white">
-              AI-powered Marketing Tools
+              Microfabrication & Engineering Services
             </span>
           </span>
         </h2>
       </div>
 
-      {/* Cards Grid */}
       <div className="grid gap-8 sm:grid-cols-2 lg:grid-cols-3">
         {services.map((service) => (
           <ProductCard
