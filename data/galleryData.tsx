96995146796287381affd0ba41b4a193882d2b2d
Deepak Bhatter <120327568+deepak5512@users.noreply.github.com>
2025-11-08 22:38:58 +0530

    New Images Added

--- data/galleryData.tsx
+++ data/galleryData.tsx
@@ -1,4 +1,5 @@
 export const galleryData = [
+  // Group 1 (2,1,1)
   {
     id: 1,
     src: "/gallery/image1.jpg",
@@ -22,12 +23,14 @@ export const galleryData = [
       "With Hon’ble Cabinet Minister, Odisha Shri Ashok Chandra Panda at Global Bio India 2023, Delhi",
     span: "col-span-1",
   },
+
+  // Group 2 (1,2,1)
   {
     id: 4,
     src: "/gallery/image4.jpg",
     alt: "Team Sarbit Innovations",
     title: "Team Sarbit Innovations",
-    span: "col-span-2",
+    span: "col-span-1",
   },
   {
     id: 5,
@@ -36,5 +39,219 @@ export const galleryData = [
     title:
       "Showcasing Our Prototype at Science & Technology, Jodhpur Cluster Fair",
     span: "col-span-2",
+  },
+  {
+    id: 6,
+    src: "/gallery/image6.jpg",
+    alt: "Showcasing Our Prototype",
+    title: "Team Sarbit at Startup Mahakumbh (April 2025)",
+    span: "col-span-1",
+  },
+
+  // Group 3 (1,1,2)
+  {
+    id: 7,
+    src: "/gallery/image8.jpg",
+    alt: "Showcasing Our Prototype",
+    title: "Team Sarbit at Startup Mahakumbh (April 2025)",
+    span: "col-span-2",
+  },
+  {
+    id: 8,
+    src: "/gallery/image9.jpg",
+    alt: "Showcasing Our Prototype",
+    title: "Team Sarbit at Startup Mahakumbh (April 2025)",
+    span: "col-span-1",
+  },
+  {
+    id: 9,
+    src: "/gallery/image10.jpg",
+    alt: "Showcasing Our Prototype",
+    title: "Team Sarbit at Global Bio India Sept 2024",
+    span: "col-span-1",
+  },
+
+  // Group 4 (2,1,1)
+  {
+    id: 11,
+    src: "/gallery/image11.jpg",
+    alt: "Showcasing Our Prototype",
+    title: "Team Sarbit at Global Bio India Sept 2024",
+    span: "col-span-2",
+  },
+  {
+    id: 12,
+    src: "/gallery/image12.jpg",
+    alt: "Showcasing Our Prototype",
+    title: "Team Sarbit at Global Bio India Sept 2024",
+    span: "col-span-1",
+  },
+  {
+    id: 13,
+    src: "/gallery/image13.jpg",
+    alt: "Showcasing Our Prototype",
+    title: "Team Sarbit at Global Bio India Sept 2024",
+    span: "col-span-1",
+  },
+
+  // Group 5 (1,2,1)
+  {
+    id: 14,
+    src: "/gallery/image14.jpg",
+    alt: "Showcasing Our Prototype",
+    title: "In interaction with Shri Ambarish Kumar, IAS",
+    span: "col-span-1",
+  },
+  {
+    id: 16,
+    src: "/gallery/image16.jpg",
+    alt: "Showcasing Our Prototype",
+    title: "In interaction with Shri Ambarish Kumar, IAS",
+    span: "col-span-2",
+  },
+  {
+    id: 17,
+    src: "/gallery/image17.jpg",
+    alt: "Showcasing Our Prototype",
+    title: "MoU signing ceremony with Sangam University, Bhilwara",
+    span: "col-span-1",
+  },
+
+  // Group 6 (1,1,2)
+  {
+    id: 18,
+    src: "/gallery/image18.png",
+    alt: "Showcasing Our Prototype",
+    title: "MoU signing ceremony with Sangam University, Bhilwara",
+    span: "col-span-1",
+  },
+  {
+    id: 19,
+    src: "/gallery/image19.jpg",
+    alt: "Showcasing Our Prototype",
+    title: "MoU signing ceremony with Sangam University, Bhilwara",
+    span: "col-span-2",
+  },
+  {
+    id: 20,
+    src: "/gallery/image20.jpg",
+    alt: "Showcasing Our Prototype",
+    title: "MoU signing ceremony with Sangam University, Bhilwara",
+    span: "col-span-1",
+  },
+
+  // Group 7 (2,1,1)
+  {
+    id: 21,
+    src: "/gallery/image21.jpg",
+    alt: "Showcasing Our Prototype",
+    title: "Invoking the Gods before invoking the project. (January 2024)",
+    span: "col-span-2",
+  },
+  {
+    id: 22,
+    src: "/gallery/image22.jpg",
+    alt: "Showcasing Our Prototype",
+    title: "Invoking the Gods before invoking the project. (January 2024)",
+    span: "col-span-1",
+  },
+  {
+    id: 23,
+    src: "/gallery/image23.jpg",
+    alt: "Showcasing Our Prototype",
+    title: "Invoking the Gods before invoking the project. (January 2024)",
+    span: "col-span-1",
+  },
+
+  // Group 8 (1,2,1)
+  {
+    id: 24,
+    src: "/gallery/image24.jpg",
+    alt: "Showcasing Our Prototype",
+    title: "Invoking the Gods before invoking the project. (January 2024)",
+    span: "col-span-1",
+  },
+  {
+    id: 25,
+    src: "/gallery/image25.jpg",
+    alt: "Showcasing Our Prototype",
+    title: "Invoking the Gods before invoking the project. (January 2024)",
+    span: "col-span-2",
+  },
+  {
+    id: 26,
+    src: "/gallery/image26.jpg",
+    alt: "Showcasing Our Prototype",
+    title: "Invoking the Gods before invoking the project. (January 2024)",
+    span: "col-span-1",
+  },
+
+  // Group 9 (1,1,2)
+  {
+    id: 27,
+    src: "/gallery/image27.jpg",
+    alt: "Showcasing Our Prototype",
+    title: "Interns at work",
+    span: "col-span-1",
+  },
+  {
+    id: 28,
+    src: "/gallery/image28.jpg",
+    alt: "Showcasing Our Prototype",
+    title: "Interns at work",
+    span: "col-span-2",
+  },
+  {
+    id: 29,
+    src: "/gallery/image29.jpg",
+    alt: "Showcasing Our Prototype",
+    title: "Interns at work",
+    span: "col-span-1",
+  },
+
+  // Group 10 (2,1,1)
+  {
+    id: 30,
+    src: "/gallery/image30.jpg",
+    alt: "Showcasing Our Prototype",
+    title: "Interns at work",
+    span: "col-span-2",
+  },
+  {
+    id: 31,
+    src: "/gallery/image31.jpg",
+    alt: "Showcasing Our Prototype",
+    title: "Interns at work",
+    span: "col-span-1",
+  },
+  {
+    id: 32,
+    src: "/gallery/image32.jpg",
+    alt: "Showcasing Our Prototype",
+    title: "Team Sarbit at Semicon India 2024",
+    span: "col-span-1",
+  },
+
+  // Group 11 (1,2,1)
+  {
+    id: 33,
+    src: "/gallery/image33.jpg",
+    alt: "Showcasing Our Prototype",
+    title: "Team Sarbit at Semicon India 2024",
+    span: "col-span-1",
+  },
+  {
+    id: 34,
+    src: "/gallery/image34.jpg",
+    alt: "Showcasing Our Prototype",
+    title: "Team Sarbit at Semicon India 2024",
+    span: "col-span-2",
+  },
+  {
+    id: 35,
+    src: "/gallery/image35.jpg",
+    alt: "Showcasing Our Prototype",
+    title: "Team Sarbit at Semicon India 2024",
+    span: "col-span-1",
   },
 ];
