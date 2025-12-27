c115ea668621043345f0359fc8addf2dc8d9b67b
Deepak Bhatter <120327568+deepak5512@users.noreply.github.com>
2025-12-27 22:09:58 +0530

    Gallery Modified

--- data/galleryData.tsx
+++ data/galleryData.tsx
@@ -1,5 +1,5 @@
 export const galleryData = [
-  // Group 1 (2,1,1)
+  // Group 1 (1,1,2)
   {
     id: 1,
     src: "/gallery/image1.jpg",
@@ -8,47 +8,12 @@ export const galleryData = [
     span: "col-span-1",
   },
   {
-    id: 2,
-    src: "/gallery/image2.jpg",
-    alt: "With Global Business leader Kiran Mazumdar Shaw",
-    title:
-      "With Global Business leader Kiran Mazumdar Shaw at Global Bio India 2023, Delhi",
-    span: "col-span-2",
-  },
-  {
-    id: 3,
-    src: "/gallery/image3.jpg",
-    alt: "With Honble Cabinet Minister, Odisha Shri Ashok Chandra Panda",
-    title:
-      "With Hon’ble Cabinet Minister, Odisha Shri Ashok Chandra Panda at Global Bio India 2023, Delhi",
-    span: "col-span-1",
-  },
-
-  // Group 2 (1,2,1)
-  {
-    id: 4,
-    src: "/gallery/image4.jpg",
-    alt: "Team Sarbit Innovations",
-    title: "Team Sarbit Innovations",
-    span: "col-span-1",
-  },
-  {
-    id: 5,
-    src: "/gallery/image5.jpg",
-    alt: "Showcasing Our Prototype",
-    title:
-      "Showcasing Our Prototype at Science & Technology, Jodhpur Cluster Fair",
-    span: "col-span-2",
-  },
-  {
     id: 6,
     src: "/gallery/image6.jpg",
     alt: "Showcasing Our Prototype",
     title: "Team Sarbit at Startup Mahakumbh (April 2025)",
     span: "col-span-1",
   },
-
-  // Group 3 (1,1,2)
   {
     id: 7,
     src: "/gallery/image8.jpg",
@@ -56,6 +21,9 @@ export const galleryData = [
     title: "Team Sarbit at Startup Mahakumbh (April 2025)",
     span: "col-span-2",
   },
+  
+
+  // Group 2 (1,2,1)
   {
     id: 8,
     src: "/gallery/image9.jpg",
@@ -68,17 +36,17 @@ export const galleryData = [
     src: "/gallery/image10.jpg",
     alt: "Showcasing Our Prototype",
     title: "Team Sarbit at Global Bio India Sept 2024",
-    span: "col-span-1",
+    span: "col-span-2",
   },
-
-  // Group 4 (2,1,1)
   {
     id: 11,
     src: "/gallery/image11.jpg",
     alt: "Showcasing Our Prototype",
     title: "Team Sarbit at Global Bio India Sept 2024",
-    span: "col-span-2",
+    span: "col-span-1",
   },
+
+  // Group 3 (1,1,2)
   {
     id: 12,
     src: "/gallery/image12.jpg",
@@ -93,15 +61,15 @@ export const galleryData = [
     title: "Team Sarbit at Global Bio India Sept 2024",
     span: "col-span-1",
   },
-
-  // Group 5 (1,2,1)
   {
     id: 14,
     src: "/gallery/image14.jpg",
     alt: "Showcasing Our Prototype",
     title: "In interaction with Shri Ambarish Kumar, IAS",
-    span: "col-span-1",
+    span: "col-span-2",
   },
+
+  // Group 4 (2,1,1)
   {
     id: 16,
     src: "/gallery/image16.jpg",
@@ -116,8 +84,6 @@ export const galleryData = [
     title: "MoU signing ceremony with Sangam University, Bhilwara",
     span: "col-span-1",
   },
-
-  // Group 6 (1,1,2)
   {
     id: 18,
     src: "/gallery/image18.png",
@@ -125,29 +91,31 @@ export const galleryData = [
     title: "MoU signing ceremony with Sangam University, Bhilwara",
     span: "col-span-1",
   },
+
+  // Group 5 (1,2,1)
   {
     id: 19,
     src: "/gallery/image19.jpg",
     alt: "Showcasing Our Prototype",
     title: "MoU signing ceremony with Sangam University, Bhilwara",
-    span: "col-span-2",
+    span: "col-span-1",
   },
   {
     id: 20,
     src: "/gallery/image20.jpg",
     alt: "Showcasing Our Prototype",
     title: "MoU signing ceremony with Sangam University, Bhilwara",
-    span: "col-span-1",
+    span: "col-span-2",
   },
-
-  // Group 7 (2,1,1)
   {
     id: 21,
     src: "/gallery/image21.jpg",
     alt: "Showcasing Our Prototype",
     title: "Invoking the Gods before invoking the project. (January 2024)",
-    span: "col-span-2",
+    span: "col-span-1",
   },
+
+  // Group 6 (1,1,2)
   {
     id: 22,
     src: "/gallery/image22.jpg",
@@ -162,15 +130,15 @@ export const galleryData = [
     title: "Invoking the Gods before invoking the project. (January 2024)",
     span: "col-span-1",
   },
-
-  // Group 8 (1,2,1)
   {
     id: 24,
     src: "/gallery/image24.jpg",
     alt: "Showcasing Our Prototype",
     title: "Invoking the Gods before invoking the project. (January 2024)",
-    span: "col-span-1",
+    span: "col-span-2",
   },
+
+  // Group 7 (2,1,1)
   {
     id: 25,
     src: "/gallery/image25.jpg",
@@ -185,8 +153,6 @@ export const galleryData = [
     title: "Invoking the Gods before invoking the project. (January 2024)",
     span: "col-span-1",
   },
-
-  // Group 9 (1,1,2)
   {
     id: 27,
     src: "/gallery/image27.jpg",
@@ -194,29 +160,31 @@ export const galleryData = [
     title: "Interns at work",
     span: "col-span-1",
   },
+
+  // Group 8 (1,2,1)
   {
     id: 28,
     src: "/gallery/image28.jpg",
     alt: "Showcasing Our Prototype",
     title: "Interns at work",
-    span: "col-span-2",
+    span: "col-span-1",
   },
   {
     id: 29,
     src: "/gallery/image29.jpg",
     alt: "Showcasing Our Prototype",
     title: "Interns at work",
-    span: "col-span-1",
+    span: "col-span-2",
   },
-
-  // Group 10 (2,1,1)
   {
     id: 30,
     src: "/gallery/image30.jpg",
     alt: "Showcasing Our Prototype",
     title: "Interns at work",
-    span: "col-span-2",
+    span: "col-span-1",
   },
+
+  // Group 9 (1,1,2)
   {
     id: 31,
     src: "/gallery/image31.jpg",
@@ -231,15 +199,15 @@ export const galleryData = [
     title: "Team Sarbit at Semicon India 2024",
     span: "col-span-1",
   },
-
-  // Group 11 (1,2,1)
   {
     id: 33,
     src: "/gallery/image33.jpg",
     alt: "Showcasing Our Prototype",
     title: "Team Sarbit at Semicon India 2024",
-    span: "col-span-1",
+    span: "col-span-2",
   },
+
+  // Group 10 (2,1,1)
   {
     id: 34,
     src: "/gallery/image34.jpg",
@@ -252,6 +220,39 @@ export const galleryData = [
     src: "/gallery/image35.jpg",
     alt: "Showcasing Our Prototype",
     title: "Team Sarbit at Semicon India 2024",
+    span: "col-span-1",
+  },
+  {
+    id: 2,
+    src: "/gallery/image2.jpg",
+    alt: "With Global Business leader Kiran Mazumdar Shaw",
+    title:
+      "With Global Business leader Kiran Mazumdar Shaw at Global Bio India 2023, Delhi",
+    span: "col-span-1",
+  },
+
+  // Group 11 (1,2,1)
+  {
+    id: 3,
+    src: "/gallery/image3.jpg",
+    alt: "With Honble Cabinet Minister, Odisha Shri Ashok Chandra Panda",
+    title:
+      "With Hon’ble Cabinet Minister, Odisha Shri Ashok Chandra Panda at Global Bio India 2023, Delhi",
+    span: "col-span-1",
+  },
+  {
+    id: 4,
+    src: "/gallery/image4.jpg",
+    alt: "Team Sarbit Innovations",
+    title: "Team Sarbit Innovations",
+    span: "col-span-2",
+  },
+  {
+    id: 5,
+    src: "/gallery/image5.jpg",
+    alt: "Showcasing Our Prototype",
+    title:
+      "Showcasing Our Prototype at Science & Technology, Jodhpur Cluster Fair",
     span: "col-span-1",
   },
 ];
